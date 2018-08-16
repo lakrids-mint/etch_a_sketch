ea8cabd2de78119088e3c0940ac53ba551ab3acd
Mette Hansen <mettepernille@hotmail.com>
2018-08-16 20:42:54 +0200

    Last version - minor changes

--- script.js
+++ script.js
@@ -1,56 +1,48 @@
+//Global variables
 const grid = document.getElementById("grid");
 const gridSize = 500;
 let cellSize;
-let cells = document.querySelectorAll(".cells");
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", resetGrid);
 
+
+//calculates cell size => returns number
 function setCellSize(width){
-  cellSize = Math.floor((width/gridSize)* 1000);
-
+  cellSize = Math.floor(gridSize/width);
   return cellSize;
 }
-let color;
-let colors = ["#D99F7E", "#EFDFC8", "#6A91B2", "#2C0216", "#590A27"];
-function pickColor(colors){
-  color = colors[Math.floor(Math.random() * colors.length)];
-  console.log(color);
-  return color;
 
-}
+//Make grid
 function gridMaker(width){
-
   for(let i=1; i<=width*width;i++){
-    //debugger;
     let cell = document.createElement("div");
     grid.appendChild(cell);
-    cell.innerHTML = i;
-
     cell.setAttribute("class", "cells");
     setCellSize(width);
     cell.style.setProperty('--width', cellSize+"px");
     cell.style.setProperty('--height', cellSize+"px");
-
+    //this probably should be somewhere else
     cell.addEventListener("mouseover", ()=>cell.style.backgroundColor = "#da6177");
   }
 }
-gridMaker(32);
+gridMaker(16);
 
 
 
 ////////////Reset
 function resetGrid(){
-  let input = prompt("Input number of cells or something!");
-
-  let parent = document.getElementById("grid");
-  let divList = parent.children;
-
-
-  while (0<divList.length){
-
-    parent.removeChild(divList[0]);
-
+  //no number validation, please just input numbers
+  let input = prompt("Input number between 10 and 64");
+  if(input<10){
+    alert("Please input a number higher than 9");
+  }else if(input>64){
+    alert("Please input a number lower than 65")
+  }else{
+    let parent = document.getElementById("grid");
+    let divList = parent.children;
+    while (0<divList.length){
+      parent.removeChild(divList[0]);
+    }
+  gridMaker(input);
   }
-  gridMaker(input, input);
 }
-//--------------------------------------------------------------------------
-let resetButton = document.getElementById("resetButton");
-resetButton.addEventListener("click", resetGrid);
